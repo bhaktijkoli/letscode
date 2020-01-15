9b88df4e5b8e4bab30c89e3dd3c2947eaf2474e9
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-01-16 02:58:13 +0530

    Added Ace, compile api

--- client/js/app/Editor/Editor.js
+++ client/js/app/Editor/Editor.js
@@ -7,20 +7,17 @@ import "ace-builds/src-min-noconflict/ext-language_tools"
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-monokai";
 
-function onChange(newValue) {
-  console.log("change", newValue);
-}
-
 class Editor extends Component {
   constructor(props) {
     super(props);
     let lang = null;
     languages.forEach(l => {
       if(l.slug == this.props.match.params.slug)
-        lang = l;
+      lang = l;
     });
     this.state = {
-      lang
+      lang,
+      code: '',
     }
     document.title = `${lang.name} Editor`;
   }
@@ -28,11 +25,45 @@ class Editor extends Component {
     return(
       <React.Fragment>
         <div id="editor-nav">
-          
+          <div className="editor-title">
+            <h1>{this.state.lang.name}</h1>
+          </div>
+          <div className="editor-action">
+            <button className="btn btn-success btn-sm" onClick={this.onRun}>RUN</button>
+          </div>
         </div>
-      </React.Fragment>
-    );
+        <AceEditor
+          mode={this.state.lang.mode}
+          theme="monokai"
+          onLoad={this.onLoad}
+          onChange={code => this.setState({code})}
+          fontSize={14}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          value={this.state.code}
+          className="ace-editor"
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}/>
+        <div className="ace-output"></div>
+        </React.Fragment>
+      );
+    }
+    onRun = () => {
+      let data = {
+        content: this.state.code,
+        mode: this.state.lang.mode,
+      };
+      axios.post('https://compiler.bhaktijkoli.com/compile', data)
+      .then(res => {
+        $('.ace-output').append(`<p>${res.data.output}</p>`);
+      })
+    }
   }
-}
 
-export default Editor;
+  export default Editor;
